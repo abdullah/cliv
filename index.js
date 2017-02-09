f36da40ef9e0550e7e67763b7d9d34738bc19ca1
abdullah <writer-edi@hotmail.com>
2017-02-09 19:00:34 +0300

    New version

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var shell = require('shelljs');
 
 
 program
-  .version('1.0.4')
+  .version('1.0.6')
   .usage('[componentNames]')
   .option('-c, --components', 'Components name')
   .option('-f, --folder', 'Folder name')
