bcc8251ca97bea1fae883a56ed7819ef77fb6c0b
abdullah <writer-edi@hotmail.com>
2017-02-09 18:51:05 +0300

    Support folder option and multiple files

--- index.js
+++ index.js
@@ -1,34 +1,68 @@
-'use strict';
+#!/usr/bin/env node
 
-const process = require('process');
-
-const program 	= require('commander');
-const path 		= require('path');
-const cwd 		= process.cwd();
-const _PCK_JSON = path.join(cwd ,'/package.json');
-const app 		= require(_PCK_JSON);
-const fs 		= require('fs');
-const colors 	= require('colors');
-
-
-const template = fs.readFileSync(path.join(__dirname,'template.vue'), { encoding: 'utf-8' });
-const _path = app.cliv_path ? path.join(cwd,app.cliv_path) : path.join(cwd,"/src/components");
+var program = require('commander');
+var commandLineArgs = require('command-line-args')
+var path = require('path');
+var fs = require('fs');
+var colors 	= require('colors');
+var shell = require('shelljs');
 
 
 program
-  .version(app.version)
-  .option('[name]', 'Add new components')
-  .parse(process.argv);
- 
+  .version('1.0.4')
+  .usage('[componentNames]')
+  .option('-c, --components', 'Components name')
+  .option('-f, --folder', 'Folder name')
+  .parse(process.argv)
 
- if (program.args.length) {
- 	program.args.map(function(index, elem) {
-		const file = path.join(_path,index+'.vue');
-		fs.writeFile(file,template,function (e) {
-			if (e) console.log("Oh no !",e)
-			console.log("Success",file.green);	
-		})
- 	})
- }else{
- 	console.log("Please component name mate!")
- }
+
+var args = program.args
+var cwd = process.cwd();
+
+
+var optionDefinitions = [
+  { name: 'components', alias: 'c', type: String, multiple: true },
+  { name: 'folder', alias: 'f', type: String}
+]
+
+var options = commandLineArgs(optionDefinitions)
+
+if (options.components && options.components.length) {
+  createComponent()
+}else{
+  console.log("You should type the component name".red)
+}
+
+
+function createComponent() {
+
+  var componentNames = options.components;
+  var optionFolder = options.folder;
+
+  var clivTemplate = fs.readFileSync(path.join(__dirname,'template.vue'), { encoding: 'utf-8' });
+  var componentPath = path.join(cwd,optionFolder ? optionFolder : "/src/components");
+  
+
+  if (!fs.existsSync(componentPath)){
+	shell.mkdir('-p', componentPath);
+  }
+
+  componentNames.forEach( function(fileName) {
+  	  var file = path.join(componentPath , fileName +  '.vue');
+  	  
+  	  	if (!fs.existsSync(file)) {
+		  
+		  fs.writeFile(file,clivTemplate,function (e) {
+		      if (e) console.log(e)
+
+		      console.log(("✓ "+fileName+'.vue').green);  
+		  })
+
+		}else{
+		      console.log(("File is exist => ").red + file);  
+		} 
+
+  });
+
+
+}
